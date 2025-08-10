cdea0be427fa14607921f85993a047ae49edbe62
Nitin Singh <122776086+Nitin-cyperpunk@users.noreply.github.com>
2025-08-10 21:06:50 +0530

    Time: 1 ms (60.38%) | Memory: 55.2 MB (18.87%) - LeetSync

---
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var freqAlphabets = function(s) {
+    return s
+          .match(/\d{2}(?=#)|\d/g)
+          .map(num => String.fromCharCode(96 + +num))
+          .join('')
+};
